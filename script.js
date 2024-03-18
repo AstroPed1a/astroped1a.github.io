5ed1817d7ea1d6dc9ccee50060258eac6d641566
Shjvr <131300893+Shjvr@users.noreply.github.com>
2024-03-18 10:21:13 -0700

    javascript is ass

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function toggleDropdown() {
   let dropdown = document.getElementById("myDropdown");
   dropdown.classList.toggle("active");
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-button')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('active')) {
+        openDropdown.classList.remove('active');
+      }
+    }
+  }
+}
